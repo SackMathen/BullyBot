a607ea5a7f257c5689d210d80bb771de339c4b73
Kyler Farrar <kaf386@msstate.edu>
2021-09-06 17:04:22 -0500

    Testing role builder

--- commands/addmod.js
+++ commands/addmod.js
@@ -19,8 +19,9 @@ class AddMod {
                     return await interaction.reply("That role is already a moderator!");
                 } else {
                     json_mods.mods.push(role.id);
-                    fs.writeFileSync("persistent/moderators.json", JSON.stringify(json_mods));     
-                    return await interaction.reply(`${roleMention(role.name)} has been added to the moderator list!`);          
+                    fs.writeFileSync("persistent/moderators.json", JSON.stringify(json_mods));
+                    const role_mention = roleMention(role.name)
+                    return await interaction.reply(`${role_mention} has been added to the moderator list!`);          
                 }
             } else {
                 return await interaction.reply("Only moderators and administrators can use this command!");
