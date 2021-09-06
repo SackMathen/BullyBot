48937c70a6c763e53dc3ee8491b00c743d97900d
Kyler Farrar <kaf386@msstate.edu>
2021-09-06 17:06:16 -0500

    See previous

--- commands/addmod.js
+++ commands/addmod.js
@@ -20,7 +20,7 @@ class AddMod {
                 } else {
                     json_mods.mods.push(role.id);
                     fs.writeFileSync("persistent/moderators.json", JSON.stringify(json_mods));
-                    return await interaction.reply(roleMention(role.name) + "has been added to the moderator list!");          
+                    return await interaction.reply(roleMention(role.name) + " has been added to the moderator list!");          
                 }
             } else {
                 return await interaction.reply("Only moderators and administrators can use this command!");
