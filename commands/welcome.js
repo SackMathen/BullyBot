f7969cb60178a708c79bd444d48301b717603f0c
jytole <kylerssmith@hotmail.com>
2021-09-06 22:51:40 -0500

    Began implementing the branching welcome command

--- commands/welcome.js
+++ commands/welcome.js
@@ -2,34 +2,54 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const { isElevated } = require("../helpers/elevation_check");
 const fs = require('fs');
 
-class Welcome {
+class Welcome
+{
     constructor() { }
-    command() {
+    command()
+    {
+        //Build the command w/ all its fields for discord
         const api_command = new SlashCommandBuilder().setName("welcome")
             .setDescription("Manages the welcome message configuration for this server.")
-            //.addChannelOption(option => option.setName().setDescription().setRequired(true).addChoice())
-            .addChannelOption(option => option.setName("option").setDescription("The channel to send welcome messages to").setRequired(true));
+            .addIntegerOption(option => option.setName("subcommand").setDescription("The branch of the welcome command you wish to activate").setRequired(true).addChoice("Toggle Enable", 0).addChoice("Set Channel", 1).addChoice("Edit Message", 2))
+            .addStringOption(option => option.setName("welcomemessage").setDescription("The welcome message to be sent when a user joins").setRequired(false))
+            .addChannelOption(option => option.setName("channel").setDescription("The channel to send welcome messages to").setRequired(false));
         return api_command;
     }
-    async execute(interaction) {
-        if (interaction.commandName === 'watchthread') {
-            if (isElevated(interaction.member)) {
-                const thread = interaction.options.getChannel('thread');
-                if (thread.isThread()) {
-                    const json_threads = JSON.parse(fs.readFileSync("persistent/permathreads.json"));
-                    json_threads.threads.push(thread.id);
-                    fs.writeFileSync("persistent/permathreads.json", JSON.stringify(json_threads));
-                    return await interaction.reply(`Now watching the thread: ${thread.name}!`);
-                } else {
-                    return await interaction.reply("Watch Thread is for threads silly rabit!");
+    async execute(interaction) 
+    {
+        if (interaction.commandName === 'welcome')
+        {
+            if (isElevated(interaction.member))
+            {
+                const subcommand = interaction.options.getInteger('subcommand');
+                const welcomemessage = interaction.options.getString('welcomemessage');
+                const channel = interaction.options.getChannel('channel');
+                switch(subcommand)
+                {
+                    //Toggle Enable
+                    case 0:
+                        break;
+                    //Set Channel
+                    case 1:
+                        const json_welcome = JSON.parse(fs.readFileSync("persistent/welcome.json"));
+                        json_welcome.threads.push(thread.id);
+                        fs.writeFileSync("persistent/permathreads.json", JSON.stringify(json_threads));
+                        return await interaction.reply(`Now watching the thread: ${thread.name}!`);
+                        break;
+                    
+                    //Edit Message
+                    case 2:
+                        break;
                 }
-            } else {
+            }
+            else
+            {
                 return await interaction.reply("Only moderators and administrators can use this command!");
             }
         }
     };
 }
 
-inst_watch = new WatchThread();
+inst_welcome = new Welcome();
 
-module.exports = inst_watch;
+module.exports = inst_welcome;
